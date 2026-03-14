13f8a886690c824fd39cfc99bfa290e6d1e3e2c7
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-03-14 12:49:57 +0100

    Initial project structure

--- pisarz-clicker-firmware/src/main.cpp
+++ pisarz-clicker-firmware/src/main.cpp
@@ -2,18 +2,65 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define SERVOMIN  120 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  520 // This is the 'maximum' pulse length count (out of 4096)
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define SERVOMIN  120
+#define SERVOMAX  520
+#define SERVO_FREQ 50
+
+//
+// Drivers objects
+//
 
 Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver();
+
+//
+// Data types
+//
+
+typedef struct{
+    uint8_t driver;
+    uint8_t channel;
+    uint8_t dir;
+} Key;
+
+typedef enum{
+    BORN,
+    CLICKING,
+    RETURNING,
+    DONE
+} clickEventState;
+
+typedef struct{
+    Key key;
+    unsigned long bornTime;
+    clickEventState state;
+
+} ClickEvent;
+
+
+ClickEvent clickEventTab[100];
+uint8_t clickEventTabLen = 0;
+
+//
+// Functions
+//
+
+void receiveData();
+void updateEvents();
+
+ClickEvent clickEvent_create(uint16_t key, unsigned long time, uint8_t *returnCode);
+void clickEvent_update(ClickEvent *event, unsigned long time);
+void servoDown(Key key);
+void servoUp(Key key);
+
 
 uint8_t servonum = 7;
 
 void setup() {
+    // Serial setup
     Serial.begin(9600);
     Serial.println("8 channel Servo test!");
 
+    // Servo drivers setup
     servoDriver.begin();
     servoDriver.setOscillatorFrequency(27000000);
     servoDriver.setPWMFreq(SERVO_FREQ);
@@ -22,12 +69,56 @@ void setup() {
 }
 
 void loop() {
-    // Drive each servo one at a time using setPWM()
-    servoDriver.setPWM(servonum, 0, SERVOMIN);
+    // servoDriver.setPWM(servonum, 0, SERVOMIN);
 
-    delay(1000);
+    // delay(1000);
   
-    servoDriver.setPWM(servonum, 0, SERVOMAX);
+    // servoDriver.setPWM(servonum, 0, SERVOMAX);
 
-    delay(1000);
+    // delay(1000);
+
+    receiveData();
+    for(int i=0; i<5; i++)
+        Serial.print(clickEventTab[i].key.channel);
+    Serial.println();
+    delay(500);
+}
+
+ClickEvent clickEvent_create(uint16_t character, unsigned long time, uint8_t *returnCode){
+    Key key;
+    *returnCode = 1;
+    if(character == 'a'){
+        key.driver = 0;
+        key.channel = 7;
+        key.dir = 0;
+        *returnCode = 0;
+    }
+    else{
+        key.driver = 0;
+        key.channel = 0;
+        key.dir = 0;
+    }
+
+    ClickEvent event;
+    event.key = key;
+    event.bornTime = time;
+    event.state = BORN;
+
+    return event;
+}
+
+void receiveData(){
+    if(Serial.available() > 0){
+        uint8_t incomingByte = Serial.read();
+
+        Serial.print("Received: ");
+        Serial.println(incomingByte, DEC);
+
+        uint8_t retCode;
+        ClickEvent event = clickEvent_create(incomingByte, millis(), &retCode);
+        if(retCode == 0){
+            clickEventTab[clickEventTabLen] = event;
+            clickEventTabLen++;
+        }
+    }
 }
