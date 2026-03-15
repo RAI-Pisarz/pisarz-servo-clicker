25c9dfc6423f50730ced55cfe0746602158a9eba
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-03-15 10:57:07 +0100

    Getting servo Dir from LUT

--- pisarz-clicker-firmware/src/key.cpp
+++ pisarz-clicker-firmware/src/key.cpp
@@ -5,20 +5,9 @@ Key key_create(uint16_t character, uint8_t *returnCode){
     Key key;
     *returnCode = 0;
 
-    // Lookup table must be inserted here instead of line of if statements
-    if(character == 'a'){
-        //key.driver = &servoDriver;
-        //key.channel = servoChannelLUT['a'-33];
-        key.dir = LEFT;
-    }
-    else if(character == 'b'){
-        //key.driver = &servoDriver;
-        //key.channel = servoChannelLUT['b'-33];
-        key.dir = LEFT;
-    }
-
     key.driver = getServoDriverFromLUT(character, returnCode);
     key.channel = getServoChannelFromLUT(character, returnCode);
+    key.dir = getServoDirFromLUT(character, returnCode);
 
     return key;
 }
--- pisarz-clicker-firmware/src/lut.h
+++ pisarz-clicker-firmware/src/lut.h
@@ -3,11 +3,13 @@
 
 #include <stdint.h>
 #include <Adafruit_PWMServoDriver.h>
+#include "key.h"
 
 extern Adafruit_PWMServoDriver servoDriver;
 
 const uint8_t getServoChannelFromLUT(uint8_t character, uint8_t *returnCode);
 const Adafruit_PWMServoDriver *getServoDriverFromLUT(uint8_t character, uint8_t *returnCode);
+const servoDir getServoDirFromLUT(uint8_t character, uint8_t *returnCode);
 
 
 #endif
